b25d675810afba0f87f9e5c034457f0f6f0fa494
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-12-14 22:52:54 -0300

    started frontend

---
+++ src/main/java/br/com/studies/cache_studies/frontend/LoginController.java
@@ -0,0 +1,17 @@
+package br.com.studies.cache_studies.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,4 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    alert('Redirecionando para a página de login...');
+    window.location.href = '/login';
+});
